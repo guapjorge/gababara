18a6764f31d12092613994c2a502484fd9a9b0a6
Burness Scott III <bsiii102798@gmail.com>
2023-06-28 19:51:24 -0700

    additions

--- server.js
+++ server.js
@@ -1,7 +1,9 @@
 const express = require('express');
 const routes = require('./routes');
+const mysql = require('mysql')
 
 const sequelize = require('./config/connection');
+const { Session } = require('inspector');
 
 const app = express();
 const PORT = process.env.PORT || 3001;
@@ -10,6 +12,29 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
 app.use(routes);
+
+const connection = mysql.createConnection({
+  host : 'localhost',
+  database: 'testing',
+  user: 'roots',
+  password: '',
+
+});
+
+connection.connection((error) => {
+  console.log("you have succesfully did the job.")
+});
+
+app.use(session({
+  secret : '',
+  resave : false,
+  
+
+}));
+
+
+
+
 
 sequelize.sync({ force: false }).then(() => {
   app.listen(PORT, () => console.log(`App listening on port ${PORT}!`));
