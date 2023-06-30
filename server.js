c7bde06e5830c814a92e7813357b4469a1589989
Aaron DeLara <aarondelara64@gmail.com>
2023-06-29 21:01:21 -0700

    did some get routes

--- server.js
+++ server.js
@@ -1,13 +1,21 @@
 const express = require('express');
-const routes = require('./routes/routes');
-
+const routes = require('./routes');
+const exphbs = require("express-handlebars")
 const sequelize = require('./config/connection');
+
+
 
 const app = express();
 const PORT = process.env.PORT || 3001;
 
+
+const hbs = exphbs.create({})
+app.engine('handlebars', hbs.engine);
+app.set('view engine', 'handlebars')
+
 app.use(express.json());
-app.use(express.urlencoded({ extended: true }));
+app.use(express.urlencoded({ extended: false }));
+app.use(express.static("public"))
 
 app.use(routes);
 
