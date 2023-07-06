680cd5e28adbe76a0a8aeeb2897d5dd6661406e7
guapjorge <alcalajorgea@gmail.com>
2023-07-06 16:50:39 -0700

    d

--- sql.sql/sql.sql
+++ sql.sql/sql.sql
@@ -3,37 +3,25 @@ create database store;
 
 use store;
 
--- create table inventory(
---     id int not null auto_increment primary key,
---     clothes_name varchar(30) not null,
---     furniture_name varchar(30) not null,
---     jewelry_name varchar(30) not null,
---     tech_name varchar(30) not null,
---     clothes_price int not null,
---     furniture_price int not null,
---     jewelry_price int not null,
---     tech_price int not null
--- );
+create table clothes (
+    id int not null auto_increment primary key,
+    clothes_name varchar(30) not null,
+    clothes_price int not null
+);
 
--- create table clothes (
---     id int not null auto_increment primary key,
---     clothes_name varchar(30) not null,
---     clothes_price int not null
--- );
-
--- create table furniture (
---     id int not null auto_increment primary key,
---     furniture_name varchar(30) not null,
---     furniture_price int not null
--- );
--- create table jewelry (
---     id int not null auto_increment primary key,
---     jewelry_name varchar(30) not null,
---     jewelry_price int not null
--- );
--- create table tech (
---     id int not null auto_increment primary key,
---     tech_name varchar(30) not null,
---     tech_price int not null
--- );
+create table furniture (
+    id int not null auto_increment primary key,
+    furniture_name varchar(30) not null,
+    furniture_price int not null
+);
+create table jewelry (
+    id int not null auto_increment primary key,
+    jewelry_name varchar(30) not null,
+    jewelry_price int not null
+);
+create table tech (
+    id int not null auto_increment primary key,
+    tech_name varchar(30) not null,
+    tech_price int not null
+);
 
